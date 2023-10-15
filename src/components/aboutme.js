43af2dcbc43a3587bbda2b1d405f49606775d21c
vasu kantesariya <kantesariyav@gmail.com>
2023-10-14 20:57:48 -0700

    adding files to repo

---
+++ src/components/aboutme.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+    <div>
+          <p>I am a full stack developer with experience in HTML, CSS, JavaScript, Node.js, and MongoDB. I am passionate about building web applications that are both user-friendly and efficient.</p>
+      <p>I have a strong understanding of the full web development stack, from front-end design to back-end development. I am also proficient in a variety of programming languages and frameworks.</p>
+      <p>I am a highly motivated and results-oriented individual with a proven track record of success. I am confident that I can make a significant contribution to your team.</p></div>
+    )
+  }
+}
+
+export default About;
